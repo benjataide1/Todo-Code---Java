23e943d160de8578bf88ea048f3ff9641afd5c74
benjataide1 <benjaminatai@gmail.com>
2024-04-23 14:53:50 -0300

    Hello World - 1

---
+++ Java For Beginners -1/HelloWorld_1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!, I'm Benjamin ");
+        System.out.println("This is another comment");
+        System.out.print("I'm 22 years old");
+    }
+
+
+}
