0fc0b2416640e00662cbaa3aad18ed0f3d6e3e22
benjataide1 <benjaminatai@gmail.com>
2024-04-23 21:46:24 -0300

    Variables Java - 2

---
+++ Java For Beginners -1/Variables_2/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        // Entero
+        int age = 22;
+
+        // Double ,decimales
+        double temperature = 17.23;
+
+        //true or false
+        boolean eat = true;
+
+        //caracter
+        char caracter = 'a';
+
+        //String
+        String text = "Hello World";
+
+    }
+}
