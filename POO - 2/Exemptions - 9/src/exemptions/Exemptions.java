9c4a2ea326fe5983b8d6ab55ef08502fdbe7e13d
benjamin <benjaminatai@gmail.com>
2024-07-13 19:30:32 -0300

    Exemptions \ Exepciones - 9

---
+++ POO - 2/Exemptions - 9/src/exemptions/Exemptions.java
@@ -0,0 +1,17 @@
+package exemptions;
+
+public class Exemptions {
+    public static void main(String[] args) {
+        int[] ages = null;
+        try {
+//            int result = 3 / 0;
+
+            ages = new int[]{2, 4, 5, 1234};
+
+        } catch (Exception e) {
+//            System.out.println("No se puede dividir por zeroooo!!!");
+            System.out.println("La posicion 4 es: " + ages[4]);
+        }
+
+    }
+}
