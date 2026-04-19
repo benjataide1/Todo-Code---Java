5ecf35bbeb22faac9975feef1370466e9a088b68
Benjamin <benjaminatai@gmail.com>
2026-04-19 01:08:51 -0300

    Deleting things 0.1

--- SpringBoot - 4/demo_myapp/src/main/java/com/example/demo_myapp/controller/TestController.java
+++ SpringBoot - 4/demo_myapp/src/main/java/com/example/demo_myapp/controller/TestController.java
@@ -1,8 +1,6 @@
 package com.example.demo_myapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //@RestController - Esto es una API o EndPoint
 @RestController // Puedo Agrega un Prefijo ("/url")
