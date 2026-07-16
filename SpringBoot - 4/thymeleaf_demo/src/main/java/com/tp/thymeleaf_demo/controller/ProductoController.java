d92cabbf016f0021500d810a555e732cb554053a
Benjamin <benjaminatai@gmail.com>
2026-07-15 23:46:52 -0300

    Agregado de th:each para recorrer una lista y mostrar en Thymeleaf

---
+++ SpringBoot - 4/thymeleaf_demo/src/main/java/com/tp/thymeleaf_demo/controller/ProductoController.java
@@ -0,0 +1,31 @@
+package com.tp.thymeleaf_demo.controller;
+
+import com.tp.thymeleaf_demo.model.Producto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ProductoController {
+
+    @GetMapping("/productos")
+    public String mostrarProductos(Model model) {
+
+        List<Producto> listaProductos = new ArrayList<>();
+
+        listaProductos.add(new Producto(1L, "Notebook IdeaPad", "Lenovo", 850000));
+        listaProductos.add(new Producto(2L, "Mouse inalámbrico", "Logitech", 25000));
+        listaProductos.add(new Producto(3L, "Teclado mecánico", "Redragon", 55000));
+        listaProductos.add(new Producto(4L, "Monitor 24 pulgadas", "Samsung", 210000));
+        listaProductos.add(new Producto(5L, "Auriculares", "Sony", 78000));
+
+        //Pasamos toda la lista
+        model.addAttribute("listaProductos", listaProductos);
+
+        return "productos";
+    }
+
+}
